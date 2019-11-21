6acf70a2fedc2674b0c36ab15bb739ce5988c9db
Dan Teixeira <danrleyt@gmail.com>
2019-11-21 11:13:13 -0300

    boilerplatting new project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Presentation from './Presentation';
+
+function App() {
+  return (
+    <div className="container">
+      <Header></Header>
+      <Presentation></Presentation>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Presentation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Presentation extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Presentation</h1>
+      </div>
+    );
+  }
+}
+
+export default Presentation;
